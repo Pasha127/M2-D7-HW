70366a5544affeab3f3646d2d56de3f2441f4d9b
Pasha127 <pasha.account@gmail.com>
2022-08-17 12:09:28 +0200

    added Modal

---
+++ script.js
@@ -0,0 +1,13 @@
+const topicDropdown = document.querySelector("#topicButton");
+const sendRequestButton = document.querySelector("#sendRequestButton");
+const sendRequest = function(){
+    alert("Request Sent!");
+}
+sendRequestButton.addEventListener('click', sendRequest);
+const changeTopic = function(e){
+    topicDropdown.innerText = e.target.innerText;
+}
+const topics = document.querySelectorAll(".dropdownTopic");
+for(topic of topics){
+    topic.addEventListener("click", changeTopic);
+}
