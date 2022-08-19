476b4c871858bf6f5f2bb45b5a6272fa91cc8b68
Pasha127 <pasha.account@gmail.com>
2022-08-19 23:14:53 +0200

    finished

--- script.js
+++ script.js
@@ -12,4 +12,20 @@ for(topic of topics){
     topic.addEventListener("click", changeTopic);
 }
 
+const summerCards = document.querySelectorAll("#welcomeSummer .card");
+for(card of summerCards){
+    const tag = document.createElement("div");
+    tag.setAttribute("class", "badge badge-danger tagPlacer");
+    tag.innerText= "HOT";
+    card.append(tag);
+    
+}
 
+const deleteCards = function(){
+    const allCards = document.querySelectorAll(".card");
+    for(card of allCards){
+        card.remove()
+    }
+}
+const deleteButton = document.querySelector("#cardDeleter");
+deleteButton.onclick = function(){ deleteCards()};
